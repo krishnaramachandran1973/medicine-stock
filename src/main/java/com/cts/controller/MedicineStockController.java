f549616ba91c9bcadad4f0cc866a65a4157e0a08
Krishna Ramachandran <krishna.ramachandran1973@gmail.com>
2021-08-07 23:21:04 +0530

    Added profiles.

--- src/main/java/com/cts/controller/MedicineStockController.java
+++ src/main/java/com/cts/controller/MedicineStockController.java
@@ -2,6 +2,8 @@ package com.cts.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 public class MedicineStockController {
+	private static final Logger logger = LoggerFactory.getLogger(MedicineStockController.class);
 
 	private final MedicineStockService service;
 
 	@GetMapping("/medicineStockInformation")
 	public ResponseEntity<?> medicineStock() {
+		logger.info("Retrieving MedicalStock");
 		List<MedicineStock> medicineStock = service.getMedicineStock();
 		return medicineStock.size() > 0 ? ResponseEntity.ok(medicineStock)
 				: ResponseEntity.noContent()
